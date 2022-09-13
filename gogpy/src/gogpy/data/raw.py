3039e295c87c1f990983019fe9edac74f7e81353
david-laugh <matda8080@gmail.com>
2022-09-13 14:37:52 +0900

    Update Usecase

--- gogpy/src/gogpy/data/raw.py
+++ gogpy/src/gogpy/data/raw.py
@@ -4,42 +4,35 @@ from datetime import date
 class Raw:
     def __init__(self, line) -> None:
         self._line = line
-        self._values = self._extract_data(self)
-        self.id = self._id()
-        self.cdate = self._cdate()
-        self.lat = self._lat()
-        self.lon = self._lon()
-        self.country_code = self._country_code()
-        self.device = self._device()
-        self.place_flag = self._place_flag()
+        self._values = self._extract_data()
 
     def _extract_data(self) -> list:
         return self._line.split(",")
 
     @property
-    def _id(self) -> str:
+    def id(self) -> str:
         return self._values[0]
 
     @property
-    def _cdate(self) -> date:
+    def cdate(self) -> date:
         return self._values[1]
 
     @property
-    def _lat(self) -> float:
+    def lat(self) -> float:
         return float(self._values[2])
 
     @property
-    def _lon(self) -> float:
+    def lon(self) -> float:
         return float(self._values[3])
 
     @property
-    def _country_code(self) -> str:
+    def country_code(self) -> str:
         return self._values[4]
 
     @property
-    def _device(self) -> str:
+    def device(self) -> str:
         return self._values[5]
 
     @property
-    def _place_flag(self) -> str:
+    def place_flag(self) -> str:
         return self._values[6]
--- gogpy/src/gogpy/usecase/processing.py
+++ gogpy/src/gogpy/usecase/processing.py
@@ -3,20 +3,28 @@ from tqdm import tqdm
 
 import h3
 
+from gogpy.data.raw import Raw
 
-if __name__ == "__main__":
-    dir_name = "./art3_USA_CA"
+
+def execute():
+    dir_name = "./examples/data/"
     os.makedirs("datalake", exist_ok=True)
+
     for filename in os.listdir(dir_name):
         with open(dir_name + "/" + filename, "r") as f:
             for i, line in tqdm(enumerate(f.readlines())):
                 if i == 0:
                     pass
                 else:
+                    raw = Raw(line)
                     h3_cell = h3.geo_to_h3(
-                        float(line.split(",")[2]), float(line.split(",")[3]), 6
+                        raw.lat, raw.lon, 6
                     )
                     with open("datalake"+"/"+h3_cell, "a") as f:
                         f.write("{},{}\n".format(
-                            float(line.split(",")[2]), float(line.split(",")[3])
+                            raw.lat, raw.lon
                         ))
+
+
+if __name__ == "__main__":
+    execute()
