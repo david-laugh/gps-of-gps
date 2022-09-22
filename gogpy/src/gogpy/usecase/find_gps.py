5d1328fc35d85854c0dd05cd516e1dd663e675b6
david-laugh <matda8080@gmail.com>
2022-09-22 20:48:26 +0900

    Update Find Gps Function

--- gogpy/src/gogpy/usecase/find_gps.py
+++ gogpy/src/gogpy/usecase/find_gps.py
@@ -1,2 +1,9 @@
+
+
 # 피타고라스 정리를 활용하여
 # 주변 n km 좌표를 구함.
+def execute(origin: tuple, target: tuple, r: float) -> bool:
+    if r**2 > (target[0] - origin[0])**2 + (target[1] - origin[1])**2:
+        return True
+    else:
+        return False
