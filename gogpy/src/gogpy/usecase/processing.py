2872b757d8ca8eeee15f65ec540559003cdaff7c
david-laugh <matda8080@gmail.com>
2022-09-24 17:52:38 +0900

    Update Data Processing

--- gogpy/src/gogpy/usecase/processing.py
+++ gogpy/src/gogpy/usecase/processing.py
@@ -7,22 +7,30 @@ from gogpy.data.raw import Raw
 
 
 def execute():
-    dir_name = "./examples/data/"
-    os.makedirs("datalake", exist_ok=True)
+    raw_dir_path = "./datalake/raw/"
+    table_dir_name = "./datalake/table/"
+    # os.makedirs("datalake", exist_ok=True)
 
-    for filename in os.listdir(dir_name):
-        with open(dir_name + "/" + filename, "r") as f:
+    for filename in os.listdir(raw_dir_path):
+        with open(raw_dir_path + filename, "r") as f:
             for i, line in tqdm(enumerate(f.readlines())):
                 if i == 0:
                     pass
                 else:
                     raw = Raw(line)
                     h3_cell = h3.geo_to_h3(
-                        raw.lat, raw.lon, 6
+                        raw.lat, raw.lon, 5
                     )
-                    with open("datalake"+"/"+h3_cell, "a") as f:
-                        f.write("{},{}\n".format(
-                            raw.lat, raw.lon
+                    with open(table_dir_name + h3_cell, "a") as f:
+                        f.write("{},{},{},{},{},{},{},{}\n".format(
+                            raw.id,
+                            raw.user_id_str,
+                            raw.user_lang,
+                            raw.lat,
+                            raw.lon,
+                            raw.country_code,
+                            raw.cdate,
+                            raw.device
                         ))
 
 
