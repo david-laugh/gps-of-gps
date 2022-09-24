3b78392422fad30c49c90ce829f5e7eb81b1cd14
david-laugh <matda8080@gmail.com>
2022-09-24 17:53:03 +0900

    Update Raw

--- gogpy/src/gogpy/data/raw.py
+++ gogpy/src/gogpy/data/raw.py
@@ -14,25 +14,33 @@ class Raw:
         return self._values[0]
 
     @property
-    def cdate(self) -> date:
+    def user_id_str(self) -> str:
         return self._values[1]
 
     @property
-    def lat(self) -> float:
-        return float(self._values[2])
+    def user_lang(self) -> date:
+        return self._values[2]
 
     @property
-    def lon(self) -> float:
+    def lat(self) -> float:
         return float(self._values[3])
 
     @property
-    def country_code(self) -> str:
-        return self._values[4]
-
-    @property
-    def device(self) -> str:
-        return self._values[5]
+    def lon(self) -> float:
+        return float(self._values[4])
 
     @property
     def place_flag(self) -> str:
+        return self._values[5]
+
+    @property
+    def country_code(self) -> str:
         return self._values[6]
+
+    @property
+    def cdate(self) -> str:
+        return self._values[7]
+
+    @property
+    def device(self) -> str:
+        return self._values[8].replace('\n','')
