277cf0ba5a965e19812e6df73af884c999f54e4d
Kim-Mingyeong <kalsrud0412@gmail.com>
2022-10-14 21:49:33 +0900

    Make home page

--- gog/src/App.js
+++ gog/src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
-import MyMap from './components/MyMap';
+import "./App.css";
+import MyMap from "./components/MyMap";
+import Home from "./screen/Home";
 
 function App() {
-    return (
-        <div className="App">
-            <MyMap />
-        </div>
-    );
+  let component;
+  switch (window.location.pathname) {
+    case "/":
+      component = <MyMap />;
+      break;
+    case "/home":
+      component = <Home />;
+      break;
+  }
+  return (
+    <>
+      <div>{component}</div>
+    </>
+  );
 }
 
 export default App;
