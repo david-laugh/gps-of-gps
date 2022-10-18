c6b9dcac1e638c8a61a12a5dfe255ae236533c85
Kim-Mingyeong <kalsrud0412@gmail.com>
2022-10-18 21:21:01 +0900

    style : Modify home style

--- gog/src/screen/Home.js
+++ gog/src/screen/Home.js
@@ -1,89 +1,87 @@
-import { useState } from "react";
+import React, { Component } from "react";
 import "./Home.css";
 import Maps from "./Maps";
 
-// var form = document.querySelector("form");
-// var log = document.querySelector("#result");
-
-// form.addEventListener(
-//   "submit",
-//   function (event) {
-//     var data = new FormData(form);
-//     var output = "";
-//     for (const entry of data) {
-//       output = output + entry[0] + "=" + entry[1] + "\r";
-//     }
-//     log.innerText = output;
-//     event.preventDefault();
-//   },
-//   false
-// );
-
-export default function Home() {
-  return (
-    <div>
+class Home extends Component {
+  render() {
+    return (
       <div className="container">
         <div className="logo">
           <h3 id="logo-txt">GPS OF GPS</h3>
         </div>
+
         <form>
-          <div className="option">
-            <div className="coordinate">
-              <div id="coordinate-txt">거리(km) :</div>
-              <br />
-              <input
-                type="range"
-                id="temp"
-                name="temp"
-                min="10"
-                max="50"
-                list="km"
-              />
-              <datalist id="km">
-                <option value="10" label="10"></option>
-                <option value="20"></option>
-                <option value="30"></option>
-                <option value="40"></option>
-                <option value="50"></option>
-              </datalist>
+          <div className="wrapper">
+            <div className="maps">
+              <Maps></Maps>
             </div>
-            <div className="angle">
-              <div id="angle-txt">각도 :</div>
-              <div>
-                <input
-                  type="radio"
-                  id="30-degree-angle"
-                  name="degree-angle"
-                  value="30-degree-angle"
-                  checked
-                ></input>
-                <label for="30-degree-angle">30</label>
-                <input
-                  type="radio"
-                  id="60-degree-angle"
-                  name="degree-angle"
-                  value="60-degree-angle"
-                ></input>
-                <label for="60-degree-angle">60</label>
-                <input
-                  type="radio"
-                  id="90-degree-angle"
-                  name="degree-angle"
-                  value="90-degree-angle"
-                ></input>
-                <label for="90-degree-angle">90</label>
+            <div className="option">
+              <div className="coordinate">
+                <div id="coordinate-txt">거리(km)</div>
+                <div id="coordinate">
+                  <input
+                    type="range"
+                    id="temp"
+                    name="temp"
+                    min="10"
+                    max="50"
+                    list="km"
+                  />
+                  <datalist id="km">
+                    <option value="10" label="10"></option>
+                    <option value="20" label="20"></option>
+                    <option value="30" label="30"></option>
+                    <option value="40" label="40"></option>
+                    <option value="50" label="50"></option>
+                  </datalist>
+                </div>
+              </div>
+              <hr />
+              <div className="angle">
+                <div id="angle-txt">각도 :</div>
+                <div id="angle">
+                  <div className="degree-angle">
+                    <input
+                      type="radio"
+                      id="30-degree-angle"
+                      name="degree-angle"
+                      value="30-degree-angle"
+                      checked
+                    ></input>
+                    <label for="30-degree-angle">30°</label>
+                  </div>
+                  <div className="degree-angle">
+                    <input
+                      type="radio"
+                      id="60-degree-angle"
+                      name="degree-angle"
+                      value="60-degree-angle"
+                    ></input>
+                    <label for="60-degree-angle">60°</label>
+                  </div>
+                  <div className="degree-angle">
+                    <input
+                      type="radio"
+                      id="90-degree-angle"
+                      name="degree-angle"
+                      value="90-degree-angle"
+                    ></input>
+                    <label for="90-degree-angle">90°</label>
+                  </div>
+                </div>
+              </div>
+              {/* angle end */}
+              <div className="white-box">
+                <button type="submit" className="btn">
+                  Submit
+                </button>
               </div>
             </div>
-            <div>
-              <button type="submit" class="btn">
-                Submit
-              </button>
-            </div>
+            {/* option end */}
           </div>
         </form>
-        {/* <pre id="result"></pre> */}
       </div>
-      <Maps></Maps>
-    </div>
-  );
+    );
+  }
 }
+export default React.memo(Home);
