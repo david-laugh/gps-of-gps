39aa9a75d1c5a0078548c5a242d363236e0f67dd
Kim-Mingyeong <kalsrud0412@gmail.com>
2022-10-18 18:00:18 +0900

    feat : Add leaflet

--- gog/src/screen/Home.js
+++ gog/src/screen/Home.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./Home.css";
+import Maps from "./Maps";
 
 // var form = document.querySelector("form");
 // var log = document.querySelector("#result");
@@ -25,7 +26,6 @@ export default function Home() {
         <div className="logo">
           <h3 id="logo-txt">GPS OF GPS</h3>
         </div>
-
         <form>
           <div className="option">
             <div className="coordinate">
@@ -40,11 +40,11 @@ export default function Home() {
                 list="km"
               />
               <datalist id="km">
-                <option value="10"></option>
+                <option value="10" label="10"></option>
                 <option value="20"></option>
                 <option value="30"></option>
                 <option value="40"></option>
-                <option value="50" label="50"></option>
+                <option value="50"></option>
               </datalist>
             </div>
             <div className="angle">
@@ -83,6 +83,7 @@ export default function Home() {
         </form>
         {/* <pre id="result"></pre> */}
       </div>
+      <Maps></Maps>
     </div>
   );
 }
