a344cac5b3ba8af8a70accb951febc19579e532c
david-laugh <matda8080@gmail.com>
2022-10-28 16:32:40 +0900

    Add Router & Server Data

--- gog/src/App.js
+++ gog/src/App.js
@@ -2,14 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 
 import "./App.css";
 import Home from "./screens/Home";
-import Maps from './screens/Result';
+import Result from './screens/Result';
 
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/Result/:distance/:angle" element={<Maps />} />
+      <Route path="/Result/:distance/:angle" element={<Result />} />
     </Routes>
   );
 };
--- gog/src/screens/result.js
+++ gog/src/screens/result.js
@@ -1,61 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Circle } from 'react-leaflet';
 import axios from 'axios';
+import { useParams, useNavigate } from 'react-router-dom';
 
-import { azimuth, haversine } from '../utilities/math.js';
-import { getCellData } from '../utilities/Cell.js';
+import "../assets/Result.css";
 
 
-function Maps(props) {
+function Result(props) {
+    const params = useParams();
+    const navigate = useNavigate();
+
     const [pos, setPos] = useState([48.06869406823506, 3.74459769969682]);
     const [zoom, _] = useState(12);
-    const [color, setColor] = useState('#000000');
     const [data, setData] = useState([]);
 
     async function _getData() {
         const response = await axios.get(
-          'http://127.0.0.1:3300/'
+          `http://127.0.0.1:3300/result/${params.distance}/${params.angle}/37/127`
         );
-        const dots = [];
         const items = response.data;
-
-        const cellData = getCellData(10, 4);
-        for (let j = 0; j < cellData.length; j++) {
-            for (let i = 0; i < items.length; i++) {
-                let distance = haversine(
-                    48.06869406823506, 3.74459769969682,
-                    items[i]["lat"], items[i]["lon"]
-                );
-                let angle = azimuth(
-                    48.06869406823506, 3.74459769969682,
-                    items[i]["lat"], items[i]["lon"]
-                );
-
-                if ( angle < cellData[j][0] && distance < cellData[j][1] ) {
-                    if ( cellData[j][2] == -1 ) {
-                        dots.push(
-                            {
-                                lat: items[i]["lat"],
-                                lon: items[i]["lon"],
-                                color: '#FF0000',
-                                radius: 200
-                            }
-                        );
-                    } else {
-                        dots.push(
-                            {
-                                lat: items[i]["lat"],
-                                lon: items[i]["lon"],
-                                color: '#000000',
-                                radius: 200
-                            }
-                        );
-                        items.splice(i, 1);
-                    }
-                }
-            }
-        }
-        setData(dots);
+        setData(items);
     }
 
     useEffect(()=>{
@@ -78,9 +42,10 @@ function Maps(props) {
                     />
                 ))}
             </MapContainer>
+            <button onClick={() => navigate(-1)}>Go back</button>
         </div>
     )
 }
 
 
-export default Maps;
+export default Result;
