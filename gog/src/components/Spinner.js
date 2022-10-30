20fc625e240abb609377bed1ab597704e555a871
david-laugh <matda8080@gmail.com>
2022-10-30 20:58:24 +0900

    Add Loading Spinner

---
+++ gog/src/components/Spinner.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import GridLoader  from "react-spinners/GridLoader";
+
+
+function Spinner(props) {
+
+    return (
+        <div
+            style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: '100vh',
+            }}
+        >
+            <GridLoader
+                color={"#7cf9df"}
+                size={50}
+                margin={30}
+                speedMultiplier={1}
+            />
+        </div>
+    )
+}
+
+
+export default Spinner;
