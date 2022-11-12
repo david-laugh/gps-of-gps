65492b1ce438d184f06fe2afb6da8cdf9c0f8b03
david-laugh <matda8080@gmail.com>
2022-11-12 21:28:55 +0900

    Add Search Screen

--- gog/src/App.js
+++ gog/src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 
 import "./App.css";
 import Home from "./screens/Home";
+import Search from './screens/Search';
 import Result from './screens/Result';
 
 
@@ -9,6 +10,7 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/Search" element={<Search />} />
       <Route path="/Result/:distance/:angle/:lat/:lon" element={<Result />} />
     </Routes>
   );
