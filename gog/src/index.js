98ae33bb60654f4e99c8cfd16950df54dbcd7366
david-laugh <matda8080@gmail.com>
2022-11-22 22:05:37 +0900

    Update App, index.js

--- gog/src/index.js
+++ gog/src/index.js
@@ -8,12 +8,9 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-  {/*Remove StricMode*/}
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
